033e72d99e2ca50abfefa53e936e07baab492ccb
volkmannp <60471729+volkmannp@users.noreply.github.com>
2020-10-25 14:19:26 +0100

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -1,7 +1,7 @@
 # Hello, world!
 #
 # This is an example function named 'hello'
-# which prints 'Hello, world!'.
+# which prints 'Hello, world!'. Which is cool.
 #
 # You can learn more about package authoring with RStudio at:
 #
